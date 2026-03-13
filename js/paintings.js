7ad98a7aaf473208ac1a67f40c7680d2ca2c527e
Joel Schultz <joelalexanderschultz@gmail.com>
2026-03-13 14:07:47 +0100

    Revert "THIS SHOULD FAIL A BUILD. Testing to on purpose make a test fail on github actions"
    This reverts commit 045ec0911cda47dd3962da94734ea61160a8f89e.

--- js/paintings.js
+++ js/paintings.js
@@ -18,6 +18,7 @@ const paintings = [
     title: "Herr och Fru Andersson",
     descKey: "desc_herrOchFruAndersson",
     size: "60 x 90 cm",
+    originalPrice: 3000,
     status: STATUS.SOLD,
   },
   {
