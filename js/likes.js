0398498c486f120f1fabaeeffe3a05955a19f0f7
vaavadevu <devikask412@gmail.com>
2026-04-29 19:32:25 +0200

    la till likes button

---
+++ js/likes.js
@@ -0,0 +1,10 @@
+// likes.js — standalone, no imports needed
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPJLdjWw8v9W4eMj62rFFn_RaXwq7FZqY",
+  authDomain: "vaavascanvas-37626.firebaseapp.com",
+  projectId: "vaavascanvas-37626",
+  storageBucket: "vaavascanvas-37626.firebasestorage.app",
+  messagingSenderId: "172065550967",
+  appId: "1:172065550967:web:51dbb1ceac232241898c72"
+};
